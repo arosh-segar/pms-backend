1ab21d08eaa49a6f42a9c18e36473cce2e6f3b3c
arosh-segar <arosh.segar@gmail.com>
2021-11-29 23:40:56 +0530

    changes made

---
+++ dist/items/items.controller.d.ts
@@ -0,0 +1,6 @@
+import { CreateItemDto } from './dto/createitem.dto';
+export declare class ItemsController {
+    findAll(): string;
+    findOne(param: any): string;
+    create(createItemDto: CreateItemDto): string;
+}
